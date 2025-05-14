bf0071768f525ada2e46e1846d1085a8415ca543
sumeyyekaya123 <kayawsumeyye@gmail.com>
2025-05-14 10:21:54 +0300

    Add files via upload

---
+++ inverting_op_amp_3D_graph.m
@@ -0,0 +1,10 @@
+A=0:-1:-50; % -R_2/R_1
+Vin = 0:1:50;
+[A_grid, Vin_grid] = meshgrid(A, Vin);
+Vout = A_grid .* Vin_grid;
+plot3(A_grid, Vin_grid, Vout);
+xlabel('A (gain)');
+ylabel('Vin (V)');
+zlabel('Vout (V)');
+title('3D Plot of Inverting Op-amp');
+grid on;
